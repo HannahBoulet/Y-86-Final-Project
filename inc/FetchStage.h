575055285df92f823b2f820e468d824a242215f4
nikolovdc <nikolovdc@appstate.edu>
2023-10-24 17:11:29 -0400

    Added dec from FetchStage

--- inc/FetchStage.h
+++ inc/FetchStage.h
@@ -8,7 +8,12 @@ class FetchStage: public Stage
 {
    private:
       //TODO: provide declarations for new methods
-      
+      uint64_t selectPC(PipeReg * freg, PipeReg * mreg, PipeReg * wreg);
+      bool need_regids(uint64_t f_icode);
+      bool need_valC(uint64_t f_icode);
+      uint64_t predictPC(uint64_t f_icode, uint64_t f_valC, uint64_t valP);
+      uint64_t PCincrement(uint64_t f_pc, bool needRegIds, bool needValC);
+
       void setDInput(PipeReg * dreg, uint64_t stat, uint64_t icode, uint64_t ifun, 
                      uint64_t rA, uint64_t rB,
                      uint64_t valC, uint64_t valP);
